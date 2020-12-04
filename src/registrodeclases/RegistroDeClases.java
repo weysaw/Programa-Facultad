854fdd21e31c48cf15acdff4ae2e1a8c96fb48fc
Kitrune <raymundoperezvaldez@gmail.com>
2020-12-03 18:37:37 -0800

    Añadido lectorTxt
    La clase permite leer los txt y devuelve un arrailist

---
+++ src/registrodeclases/RegistroDeClases.java
@@ -0,0 +1,18 @@
+
+package registrodeclases;
+
+/**
+ * Diseno para el algoritmo que recupera todos los datos de los archivos txt de clases
+ * @author Raymundo Perez Valdez
+ * @version 26 nov. 2020
+ */
+public class RegistroDeClases {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new gui().setVisible(true);
+    }
+
+}
